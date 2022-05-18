8305aec2bfa20e5eaf38d482d134202548b8e00a
crabtr26 <crabtr26@gmail.com>
2022-05-18 17:44:04 -0400

    First commit

---
+++ frontend/components/Profile.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react"
+import { useConnect, useWallet, useBalance } from "@connect2ic/react"
+
+const Profile = () => {
+  /*
+  * This how you use canisters throughout your app.
+  * "counter" is the name we pass in to <ConnectProvider canisters={{ counter }} />
+  */
+  const [wallet] = useWallet()
+  const [assets] = useBalance()
+  const { isConnected, principal, activeProvider } = useConnect({
+    onConnect: () => {
+      // Signed in
+    },
+    onDisconnect: () => {
+      // Signed out
+    }
+  })
+
+  return (
+    <div className="example">
+      {isConnected ? (
+        <>
+          <p>Principal: <span style={{ fontSize: "0.7em" }}>{principal}</span></p>
+          <p>Wallet address: <span style={{ fontSize: "0.7em" }}>{wallet ? wallet.principal : "-"}</span></p>
+          <table>
+            <tbody>
+            {assets && assets.map(asset => (
+              <tr key={asset.canisterId}>
+                <td>
+                  {asset.name}
+                </td>
+                <td>
+                  {asset.amount}
+                </td>
+              </tr>
+            ))}
+            </tbody>
+          </table>
+        </>
+      ) : (
+        <p className="example-disabled">Connect to access this example</p>
+      )}
+    </div>
+  )
+}
+
+export { Profile }
